d23fd266a0e0ed075df063df2a5d72b20f800aa2
songguo wei <1418291756@qq.com>
2024-01-09 16:28:54 +0800

    Initial Commit

---
+++ k.c/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  k.c
+//
+//  Created by 宋国伟 on 2024/1/9.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
