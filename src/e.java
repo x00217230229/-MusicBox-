9d065280f7e5927c85d9a49052b3cee4baf1d18c
songguo wei <1418291756@qq.com>
2024-03-04 21:32:15 +0800

    123445

--- src/e.java
+++ src/e.java
@@ -1,3 +1,8 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello,World!，hello sycwsnbb");
+    }
+}
 public class e {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello,，，，，，， World!");
