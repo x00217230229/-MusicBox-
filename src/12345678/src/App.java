026f722b52fa31e817f6a89380ff483fb9a0bf07
songguo wei <1418291756@qq.com>
2024-03-05 09:02:56 +0800

    aaa

--- src/12345678/src/App.java
+++
@@ -1,5 +0,0 @@
-public class e {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello,，，，，，， World!");
-        System.out.println("Hello,，，,,,， World!");
-    }
---
+++ src/12345678/src/aaa.java
@@ -0,0 +1,5 @@
+public class e {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+        System.out.println("Hello, World!");
+    }
