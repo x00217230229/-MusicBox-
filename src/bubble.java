bda95553d27b0e7c2ea5733ff4630b9ded50a849
songguo wei <1418291756@qq.com>
2024-03-05 09:16:44 +0800

    111

--- src/bubble.java
+++ src/bubble.java
@@ -1,10 +1,10 @@
-public class App {
+public class bubble {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello,World!，hello sycwsnbb");
     }
 }
 public class e {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello,，，，，，， World!");
+        System.out.println("Hello,， World!");
         System.out.println("Hello,，，,,,， World!");
     }
